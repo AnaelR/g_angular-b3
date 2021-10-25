5b5bd1d83466d9a57668cf76b7085e2b29f7cd54
AnaëlR <hey@anael-rodet.fr>
2021-10-25 15:50:03 +0200

    6.5

--- src/app/components/team/team.component.ts
+++ src/app/components/team/team.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-team',
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamComponent implements OnInit {
 
-  constructor() { }
+  dataTeam: DataTeam[]
+
+  constructor() {
+    this.dataTeam = [
+      new DataTeam(true, "../../../../../assets/images/team/team-2.png", "@Rigoberto Valenza", "You want customer to your store. Easily your coupans and has\n" +
+        "Clooger."),
+      new DataTeam(false, "../../../../../assets/images/team/team-1.png", "@Venessa Smith", "You want customer to your store. Easily your coupans and has\n" +
+        "Clooger."),
+      new DataTeam(true, "../../../../../assets/images/team/team-3.png", "@Donald Laughlin", "You want customer to your store. Easily your coupans and has\n" +
+        "Clooger.")
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+class DataTeam {
+  isActive: boolean
+  img: string
+  name: string
+  description: string
+  constructor(isActive: boolean, img: string, name: string, description: string) {
+    this.isActive = isActive
+    this.img = img
+    this.name = name
+    this.description = description
+  }
 }
