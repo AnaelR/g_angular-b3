9846e6f9875337e154160044c2237cf72af407b5
AnaëlR <hey@anael-rodet.fr>
2021-10-25 15:02:28 +0200

    6.4

--- src/app/components/services/services.component.ts
+++ src/app/components/services/services.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-services',
@@ -7,9 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServicesComponent implements OnInit {
 
-  constructor() { }
+  dataService: ServiceData[]
+
+  constructor() {
+    this.dataService = [
+      new ServiceData("ti-settings", "Digital Design", "Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+      new ServiceData("ti-palette", "Unlimited Colors", "Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+      new ServiceData("ti-stats-up", "Strategy Solutions", "Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+      new ServiceData("ti-package", "Awesome Support", "Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+      new ServiceData("ti-dashboard", "Truly Multipurpose", "Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+      new ServiceData("ti-headphone", "Easy to customize", "Ut enim ad minim veniam, quis nostrud exercitation\n" +
+        "ullamco laboris nisi ut aliquip ex ea commodo consequat."),
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+class ServiceData {
+  ico: string
+  name: string
+  description: string
+
+  constructor(ico: string, name: string, description: string) {
+    this.ico = ico
+    this.name = name
+    this.description = description
+  }
 }
